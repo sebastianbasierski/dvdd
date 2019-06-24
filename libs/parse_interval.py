3a746996f2dcf4dbd2131253b3ea348f238c00f4
Sebastian Basierski <sebastian@basierski.pl>
2019-06-24 14:24:05 +0100

    Parsing interval
    Signed-off-by: Sebastian Basierski <sebastian@basierski.pl>

---
+++ libs/parse_interval.py
@@ -0,0 +1,52 @@
+def get_sensors_parse_interval(interval):
+
+    msg = interval.split(':')
+    if msg == None:
+        return None
+
+    count = len(msg)
+    value = None
+
+    for idx in range(count):
+        temp_msg = msg[idx]
+
+        if temp_msg.endswith('s'):
+            temp_value = temp_msg[:-1]
+            try:
+                temp_value = int(temp_value)
+                if (temp_value >= 0):
+                    if value is None:
+                        value = 0
+                    value += temp_value
+                else:
+                    return None
+            except:
+                return None
+
+        elif temp_msg.endswith('m'):
+            temp_value = temp_msg[:-1]
+            try:
+                temp_value = int(temp_value)
+                temp_value *= 60
+                if (temp_value >= 0):
+                    if value is None:
+                        value = 0
+                    value += temp_value
+                else:
+                    return None
+            except:
+                return None
+
+        elif temp_msg.endswith('h'):
+            temp_value = temp_msg[:-1]
+            try:
+                temp_value = int(temp_value)
+                temp_value *= 3600
+                if (temp_value >= 0):
+                    if value is None:
+                        value = 0
+                    value += temp_value
+            except:
+                return None
+
+    return value
---
+++ tests/unit/parse_interval_test.py
@@ -0,0 +1,39 @@
+import pytest
+from libs.parse_interval import *
+
+
+def test_get_sensors_parse_interval():
+    count = get_sensors_parse_interval('30s')
+    assert count == 30
+    assert int(count) == 30
+
+    count = get_sensors_parse_interval('-2s')
+    assert count == None
+
+    count = get_sensors_parse_interval('-3')
+    assert count == None
+
+    count = get_sensors_parse_interval('1m')
+    assert count == 60
+    assert int(count) == 60
+
+    count = get_sensors_parse_interval('2m:3s')
+    assert count == 123
+    assert int(count) == 123
+
+    count = get_sensors_parse_interval('0m:12s')
+    assert count == 12
+    assert int(count) == 12
+
+    count = get_sensors_parse_interval('2h:3m:4s')
+    assert count == 7384
+    assert int(count) == 7384
+
+    count = get_sensors_parse_interval('x')
+    assert count == None
+
+    count = get_sensors_parse_interval('xs')
+    assert count == None
+
+
+
