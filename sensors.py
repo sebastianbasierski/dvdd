86c8fa7c3b916eb9454c18cd3fb3b4033592cd9f
Sebastian Basierski <sebastian@basierski.pl>
2019-06-27 12:40:58 +0100

    Simple handling 1 sensor
    Signed-off-by: Sebastian Basierski <sebastian@basierski.pl>

--- sensors.py
+++ sensors.py
@@ -2,8 +2,9 @@
 from libs.parse_config import *
 
 def main():
-    print 'Hello, world!'
-    print get_sensors_count()
+    count = get_sensors_count()
+    sensor = get_sensor_name(1)
+    done = handle_sensor(sensor)
        
 if __name__ == '__main__':
     main()
