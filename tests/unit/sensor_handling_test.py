e740a394bfd901f6e8a601796eb9adb432c31bb7
Sebastian Basierski <sebastian@basierski.pl>
2019-06-27 12:00:00 +0100

    Handling 1 sensor
    Signed-off-by: Sebastian Basierski <sebastian@basierski.pl>

---
+++ tests/unit/sensor_handling_test.py
@@ -0,0 +1,19 @@
+import pytest
+from libs.parse_config import *
+from libs.sensor_handling import *
+
+
+def test_handle_sensor():
+        
+    count = get_sensors_count()
+    assert count == '1'
+    assert int(count) == 1
+
+    sensor = get_sensor_name(1)
+    assert sensor == 'rpi_cpu_temp'
+
+    done = handle_sensor(sensor)
+    assert done == True
+
+
+
