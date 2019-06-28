133eb1bcc2fd28d1ea5478a46e7aad95751619e6
Sebastian Basierski <sebastian@basierski.pl>
2019-06-28 13:46:08 +0200

    Capture ctrl+c
    Signed-off-by: Sebastian Basierski <sebastian@basierski.pl>

--- sensors.py
+++ sensors.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
+import sys
+import signal
 from threading import Thread
 from libs.parse_config import *
 from libs.sensor_handling import *
+
+def signal_handler(sig, frame):
+    print "Your pressed ctrl + c"
+    sys.exit(0)
 
 def sensor_thread(idx):
     sensor = get_sensor_name(idx)
     done = handle_sensor(sensor)
 
 def main():
+    signal.signal(signal.SIGINT, signal_handler)
     count = get_sensors_count()
 
     t = Thread(target=sensor_thread, args=(1,))
