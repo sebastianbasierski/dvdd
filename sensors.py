fc6bdafc8fad3315d46e46a3bbf36596025d99d9
Sebastian Basierski <sebastian@basierski.pl>
2019-06-28 13:36:19 +0200

    Allow to run script as thread
    Signed-off-by: Sebastian Basierski <sebastian@basierski.pl>

--- sensors.py
+++ sensors.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
+from threading import Thread
 from libs.parse_config import *
 from libs.sensor_handling import *
 
+def sensor_thread(idx):
+    sensor = get_sensor_name(idx)
+    done = handle_sensor(sensor)
+
 def main():
     count = get_sensors_count()
-    sensor = get_sensor_name(1)
-    done = handle_sensor(sensor)
+
+    t = Thread(target=sensor_thread, args=(1,))
+    t.start()
        
 if __name__ == '__main__':
     main()
