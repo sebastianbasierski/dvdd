2dc45ada043e693bb0f49ca03a8227f084cc32a4
Sebastian Basierski <sebastian@basierski.pl>
2019-06-28 14:25:28 +0200

    Refactor sensor handling as class
    Signed-off-by: Sebastian Basierski <sebastian@basierski.pl>

--- libs/sensor_handling.py
+++ libs/sensor_handling.py
@@ -4,51 +4,51 @@ import subprocess
 from libs.parse_config import *
 from libs.parse_interval import *
 
-alive = True
+class Sensor:
+    def __init__(self, sensor):
+        self.sensor = sensor
+        self.alive = True
 
-def hs_stop():
-    global alive
+    def hs_stop(self):
+        self.alive = False
 
-    alive = False
+    def handle_sensor(self, one_time = None):
+        one_time = one_time or False
+        idx = get_sensor_idx(self.sensor)
+        script = get_sensor_script(self.sensor)
+        interval = get_sensors_parse_interval(get_sensor_interval(self.sensor))
 
-def handle_sensor(sensor, one_time = None):
-    global alive
+        if int(interval) == None:
+            return False
 
-    one_time = one_time or False
-    idx = get_sensor_idx(sensor)
-    script = get_sensor_script(sensor)
-    interval = get_sensors_parse_interval(get_sensor_interval(sensor))
-    if int(interval) == None:
-        return False
+        script_dir = '/domoticz/'
 
-    script_dir = '/domoticz/'
+        while self.alive:
+            # execute script
+            print("script " + script_dir + script)
+            normal = subprocess.call([script_dir + script, ""])
+            print("exit code " + str(normal))
 
-    while alive:
-        # execute script
-        print("script " + script_dir + script)
-        normal = subprocess.call([script_dir + script, ""])
-        print("exit code " + str(normal))
+            # sleep
+            for i in range(int(interval)):
+                # print alive value
+                print "Alive " + str(self.alive)
 
-        # sleep
-        for i in range(int(interval)):
-            # print alive value
-            print "Alive " + str(alive)
+                # sleep for a second
+                time.sleep(1)
 
-            # sleep for a second
-            time.sleep(1)
+                # check exit condition
+                if self.alive == False:
+                    return True
 
-            # check exit condition
-            if alive == False:
+            # execute script
+            print("script " + script_dir + script)
+            normal = subprocess.call([script_dir + script, ""])
+            print("exit code " + str(normal))
+
+            # exit loop
+            if one_time == True:
                 return True
 
-        # execute script
-        print("script " + script_dir + script)
-        normal = subprocess.call([script_dir + script, ""])
-        print("exit code " + str(normal))
-
-        # exit loop
-        if one_time == True:
-            return True
-
-    return True
+        return True
 
