38433f4943c14ab0f101209789df25fc890ccf82
Sebastian Basierski <sebastian@basierski.pl>
2019-06-28 14:25:28 +0200

    Refactor sensor handling as class
    Signed-off-by: Sebastian Basierski <sebastian@basierski.pl>

--- sensors.py
+++ sensors.py
@@ -6,20 +6,28 @@ from libs.parse_config import *
 from libs.sensor_handling import *
 
 thread = None
+sensor = None
 
 def signal_handler(sig, frame):
+    global sensor
+
     print "Your pressed ctrl + c"
-    hs_stop()
+    sensor.hs_stop()
     thread.join()
     sys.exit(0)
 
 def sensor_thread(idx):
-    sensor = get_sensor_name(idx)
-    done = handle_sensor(sensor)
+    global sensor
+
+    sensor = sensor.handle_sensor()
 
 def main():
     global thread
+    global sensor
+
     count = get_sensors_count()
+
+    sensor = Sensor(get_sensor_name(1))
 
     thread = Thread(target=sensor_thread, args=(1,))
     thread.start()
