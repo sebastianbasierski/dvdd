8fb4aee80c5e999f2954314fc95a8103d6fe267d
Sebastian Basierski <sebastian@basierski.pl>
2019-07-02 23:23:59 +0200

    Device idx is taken from config file. Refactored tests.
    Signed-off-by: Sebastian Basierski <sebastian@basierski.pl>

--- libs/sender.py
+++ libs/sender.py
@@ -6,10 +6,13 @@ class Sender():
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
+        self.idx = 0
+
+    def setup(self, idx):
+        self.idx = idx
 
     def send(self):
         # parameters
-        sensor_idx  = 1
         url_json = "http://172.16.2.40:8080/json.htm?type=command&param=udevice&idx="
         verbose  = 1  # set to 1 to print out information to the console 
 
@@ -19,7 +22,7 @@ class Sender():
             cpuTemp = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1000.0
 
             # use Domoticz JSON url to update
-            cmd = url_json  + str(sensor_idx) + "&nvalue=0&svalue=" + str(cpuTemp)
+            cmd = url_json + str(self.idx) + "&nvalue=0&svalue=" + str(cpuTemp)
             hf = urllib.urlopen(cmd)
             if verbose > 0:
                 print 'Sensor data: temperature = {0:0.1f}C'.format(cpuTemp)
