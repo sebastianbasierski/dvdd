caa654fda20e8e86c65d438b808a15346cbddee7
Sebastian Basierski <sebastian@basierski.pl>
2019-07-03 22:18:13 +0200

    Coping coverage report to html server
    Signed-off-by: Sebastian Basierski <sebastian@basierski.pl>

--- tools/tests.sh
+++ tools/tests.sh
@@ -11,7 +11,7 @@ ret_val=$?
 #scp -i ~/.ssh/id_rsa_cov csg.tar.gz rumcajs@basierski.pl:
 #coverage=$(cat htmlcov/cover/index.html | grep pc_cov | cut -d '>' -f 2 | cut -d '%' -f 1)
 coverage=$(cat htmlcov/index.html | grep -A 5 Total | tail -n 1 | cut -d '>' -f 2 | cut -d '<' -f 1)
-
+sudo cp htmlcov/* /var/www/html
 echo "Total coverage $coverage"
 exit $ret_val
 
