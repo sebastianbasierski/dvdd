180c96dda3a558b13bf0334fa754c20419ae75ef
Sebastian Basierski <sebastian@basierski.pl>
2019-07-04 23:21:46 +0200

    Updated tests
    Signed-off-by: Sebastian Basierski <sebastian@basierski.pl>

---
+++ switch_simple.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+while True:
+    time.sleep(1)
+    if(GPIO.input(22) == 1):
+        print('Button 1 pressed')
+    else:
+        print('Button 1 released')
