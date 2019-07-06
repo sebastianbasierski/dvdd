c704ba750bc819a2c13ddd8d0b90f2671d4f367d
Sebastian Basierski <sebastian@basierski.pl>
2019-07-06 22:13:57 +0200

    Added .travis CI file
    Signed-off-by: Sebastian Basierski <sebastian@basierski.pl>

--- tools/tests.sh
+++ tools/tests.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-#mkdir reports
-#touch reports/.coverage
-
 python -m pytest tests/ --cov=libs --cov-report=html --cov-report=annotate
 ret_val=$?
 
-#rm -rf csg.tar.gz
-#tar -zcvf csg.tar.gz reports/cover/*
-#scp -i ~/.ssh/id_rsa_cov csg.tar.gz rumcajs@basierski.pl:
-#coverage=$(cat htmlcov/cover/index.html | grep pc_cov | cut -d '>' -f 2 | cut -d '%' -f 1)
 coverage=$(cat htmlcov/index.html | grep -A 5 Total | tail -n 1 | cut -d '>' -f 2 | cut -d '<' -f 1)
 sudo cp htmlcov/* /var/www/html
 echo "Total coverage $coverage"
