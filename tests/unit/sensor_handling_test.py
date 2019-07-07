2a4447d657ee38840b46b4a80833e4c3e942a7cd
Sebastian Basierski <sebastian@basierski.pl>
2019-07-07 16:07:00 +0200

    Fixing tests for new config with dummy device
    Signed-off-by: Sebastian Basierski <sebastian@basierski.pl>

--- tests/unit/sensor_handling_test.py
+++ tests/unit/sensor_handling_test.py
@@ -6,11 +6,11 @@ from libs.sensor_handling import *
 def test_handle_sensor():
     config = Config()
     count = config.get_sensors_count()
-    assert count == '3'
-    assert int(count) == 3
+    assert count == '4'
+    assert int(count) == 4
 
     name = config.get_sensor_name(0)
-    assert name == 'rpi_cpu_temp'
+    assert name == 'dummy_sensor'
 
     sensor = Sensor(name, config)
     assert sensor is not None
@@ -22,17 +22,17 @@ def test_handle_sensor():
 def test_get_json():
     config = Config()
     count = config.get_sensors_count()
-    assert count == '3'
-    assert int(count) == 3
+    assert count == '4'
+    assert int(count) == 4
 
     name = config.get_sensor_name(0)
-    assert name == 'rpi_cpu_temp'
+    assert name == 'dummy_sensor'
 
     sensor = Sensor(name, config)
     assert sensor is not None
 
-    sensor.idx = 1
-    sensor.stype = "temperature"
+    sensor.idx = 0
+    sensor.stype = "dummy"
     json = sensor.get_json()
     assert json is not None
 
@@ -40,16 +40,16 @@ def test_get_json():
 def test_get_ds1820_json():
     config = Config()
     count = config.get_sensors_count()
-    assert count == '3'
-    assert int(count) == 3
+    assert count == '4'
+    assert int(count) == 4
 
-    name = config.get_sensor_name(0)
-    assert name == 'rpi_cpu_temp'
+    name = config.get_sensor_name(2)
+    assert name == 'rpi_ds1820'
 
     sensor = Sensor(name, config)
     assert sensor is not None
 
-    sensor.idx = 1
+    sensor.idx = 0
     json = sensor.get_ds1820_temperature_json()
     assert json is not None
 
@@ -57,10 +57,10 @@ def test_get_ds1820_json():
 def test_get_cpu_json():
     config = Config()
     count = config.get_sensors_count()
-    assert count == '3'
-    assert int(count) == 3
+    assert count == '4'
+    assert int(count) == 4
 
-    name = config.get_sensor_name(0)
+    name = config.get_sensor_name(1)
     assert name == 'rpi_cpu_temp'
 
     sensor = Sensor(name, config)
@@ -74,10 +74,10 @@ def test_get_cpu_json():
 def test_get_button_json():
     config = Config()
     count = config.get_sensors_count()
-    assert count == '3'
-    assert int(count) == 3
+    assert count == '4'
+    assert int(count) == 4
 
-    name = config.get_sensor_name(2)
+    name = config.get_sensor_name(3)
     assert name == 'rpi_button'
 
     sensor = Sensor(name, config)
