3b7970570c6fe85bbb2284d519c9de37cce4970e
Sebastian Basierski <sebastian@basierski.pl>
2019-07-16 22:50:09 +0200

    Removed 'cat' from script
    Signed-off-by: Sebastian Basierski <sebastian@basierski.pl>

--- tools/tests.sh
+++ tools/tests.sh
@@ -3,7 +3,7 @@
 python -m pytest tests/ --cov=libs --cov-report=html --cov-report=annotate
 ret_val=$?
 
-coverage=$(cat htmlcov/index.html | grep -A 5 Total | tail -n 1 | cut -d '>' -f 2 | cut -d '<' -f 1)
+coverage=$(< htmlcov/index.html grep -A 5 Total | tail -n 1 | cut -d '>' -f 2 | cut -d '<' -f 1)
 sudo cp htmlcov/* /var/www/html
 echo "Total coverage $coverage"
 exit $ret_val
