e3fca5a01cd441fe4c7142ca53f0938fa18093d6
Sebastian Basierski <sebastian@basierski.pl>
2019-07-16 22:51:31 +0200

    Fix indentation
    Signed-off-by: Sebastian Basierski <sebastian@basierski.pl>

--- libs/server.py
+++ libs/server.py
@@ -6,50 +6,50 @@ import sys
 
 
 class GP(BaseHTTPRequestHandler):
-        def __init__(self, *args):
-            self.get_method = self.get_method_dummy
-            self.post_method = self.post_method_dummy
-            BaseHTTPRequestHandler.__init__(self, *args)
+    def __init__(self, *args):
+        self.get_method = self.get_method_dummy
+        self.post_method = self.post_method_dummy
+        BaseHTTPRequestHandler.__init__(self, *args)
 
-        def _set_headers(self):
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
-        def do_HEAD(self):
-            self._set_headers()
+    def do_HEAD(self):
+        self._set_headers()
 
-        def get_method_dummy(self):
-            return 'dummy'
+    def get_method_dummy(self):
+        return 'dummy'
 
-        def pass_get_method(self, get_method):
-            self.get_method = get_method
+    def pass_get_method(self, get_method):
+        self.get_method = get_method
 
-        def do_GET(self):
-            self._set_headers()
-            print self.get_method()
-            print self.path
-            print parse_qs(self.path[2:])
-            resp = "<html><body><h1>Get Request Received!</h1></body></html>"
-            self.wfile.write(resp)
+    def do_GET(self):
+        self._set_headers()
+        print self.get_method()
+        print self.path
+        print parse_qs(self.path[2:])
+        resp = "<html><body><h1>Get Request Received!</h1></body></html>"
+        self.wfile.write(resp)
 
-        def post_method_dummy(self):
-            return 'dummy'
+    def post_method_dummy(self):
+        return 'dummy'
 
-        def pass_post_method(self, post_method):
-            self.post_method = post_method
+    def pass_post_method(self, post_method):
+        self.post_method = post_method
 
-        def do_POST(self):
-            self._set_headers()
-            form = cgi.FieldStorage(
-                fp=self.rfile,
-                headers=self.headers,
-                environ={'REQUEST_METHOD': 'POST'}
-            )
-            print form.getvalue("foo")
-            print form.getvalue("bin")
-            resp = "<html><body><h1>POST Request Received!</h1></body></html>"
-            self.wfile.write(resp)
+    def do_POST(self):
+        self._set_headers()
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST'}
+        )
+        print form.getvalue("foo")
+        print form.getvalue("bin")
+        resp = "<html><body><h1>POST Request Received!</h1></body></html>"
+        self.wfile.write(resp)
 
 
 class Server:
