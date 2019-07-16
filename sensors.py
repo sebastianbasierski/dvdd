0d770cb754b132da9c3e13b50e71acd13f896bad
Sebastian Basierski <sebastian@basierski.pl>
2019-07-16 23:07:15 +0200

    Refactored main script
    Signed-off-by: Sebastian Basierski <sebastian@basierski.pl>

--- sensors.py
+++ sensors.py
@@ -7,67 +7,63 @@ from threading import Thread
 from libs.parse_config import Config
 from libs.sensor_handling import Sensor
 
-storage = []
-server = None
-server_thread = None
 
+class Main:
+    def __init__(self):
+        self.storage = []
+        self.server = None
+        self.server_thread = None
 
-def signal_handler(sig, frame):
-    global storage
-    global server
+    def sensor_thread(self, idx):
+        self.storage[idx].get_sensor().handle_sensor()
 
-    print "Your pressed ctrl + c"
+    def local_server_thread(self):
+        self.server.run_thread()
 
-    for s in storage:
-        s.get_sensor().stop()
-        s.get_thread().join()
+    def stop(self):
+        for s in self.storage:
+            s.get_sensor().stop()
+            s.get_thread().join()
 
-    server.stop()
-    server_thread.join()
+        self.server.stop()
+        self.server_thread.join()
 
-    sys.exit(0)
+    def start(self):
+        config = Config()
+        ret = config.set_config('/etc/domoticz/domoticz.conf')
+        if ret is False:
+            print "Wrong config file set"
+            return -1
 
+        count = config.get_sensors_count()
 
-def sensor_thread(idx):
-    global storage
+        for idx in range(int(count)):
+            sensor = Sensor(config.get_sensor_name(idx), config)
+            thread = Thread(target=self.sensor_thread, args=(idx,))
+            self.storage.append(Storage(thread, sensor))
 
-    storage[idx].get_sensor().handle_sensor()
+        for s in self.storage:
+            s.get_thread().start()
 
-
-def local_server_thread():
-    global server
-
-    server.run_thread()
-
-
-def main():
-    global server_thread
-    global storage
-    global server
-
-    config = Config()
-    ret = config.set_config('/etc/domoticz/domoticz.conf')
-    if ret is False:
-        print "Wrong config file set"
-        exit(1)
-
-    count = config.get_sensors_count()
-
-    for idx in range(int(count)):
-        sensor = Sensor(config.get_sensor_name(idx), config)
-        thread = Thread(target=local_server_thread, args=(idx,))
-        storage.append(Storage(thread, sensor))
-
-    for s in storage:
-        s.get_thread().start()
-
-    server = Server(config)
-    server_thread = Thread(target=server_thread, args=())
-    server_thread.start()
+        self.server = Server(config)
+        self.server_thread = Thread(target=self.local_server_thread, args=())
+        self.server_thread.start()
+        return 0
 
 
 if __name__ == '__main__':
+    sensors = Main()
+    ret = sensors.start()
+    if ret == -1:
+        exit(1)
+
+    def signal_handler(*args):
+        print "Your pressed ctrl + c"
+        sensors.stop()
+        print "stopped sensors"
+        sys.exit(0)
+
     signal.signal(signal.SIGINT, signal_handler)
-    main()
+
     while True:
         signal.pause()
